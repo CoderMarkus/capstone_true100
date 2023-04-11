a0b418e460dce4a5c257f3bb70140980db2d7b51
Markus Wielandt <markus.wielandt1979@gmail.com>
2023-04-11 23:16:45 +0200

    add files and function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-export default function HomePage() {
+import React from "react";
+
+const Top100Page = () => {
+  // Daten für die Top-100-Liste
+  const top100List = [
+    { rank: 1, title: "Item 1" },
+    { rank: 2, title: "Item 2" },
+    // Weitere Listeneinträge hier
+  ];
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Top 100 Liste</h1>
+      <ul>
+        {top100List.map((item) => (
+          <li key={item.rank}>
+            <span>{item.rank}</span>
+            <span>{item.title}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default Top100Page;
