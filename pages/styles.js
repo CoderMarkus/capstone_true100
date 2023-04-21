b5a2e32f018f5d552e10c7ed6f6ad52aedcbca49
Markus Wielandt <markus.wielandt1979@gmail.com>
2023-04-21 08:03:37 +0200

    add upload button and minimal styling

---
+++ pages/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  background: black;
+  width: 375px;
+`;
+export const Uber = styled.h1`
+  margin: 0;
+  background-color: yellowgreen;
+  text-align: center;
+  font: icon;
+`;
+
+export const Orderli = styled.ol`
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Lila = styled.li`
+  text-align: center;
+`;
--- styles.js
+++ styles.js
@@ -9,6 +9,8 @@ export default createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: system-ui;
+    font-family: icon;
+    background: black;
+    color: ghostwhite;
   }
 `;
